e7a9217af7d2f9fd6e18fe7d983bdcc545812fc6
Peter Bates <peterbates@protonmail.com>
2017-04-19 17:32:40 -0700

    Added a map.

--- scripts/maps.R
+++ scripts/maps.R
@@ -2,6 +2,7 @@ source('~/Projects/portland_crime_analysis/scripts/crimes_by_category.R')
 
 library('ggmap')
 library('mapproj')
+library('lubridate')
 
 # get city map from stamen
 portland_map <- get_map(location = c(-122.81, 45.43, -122.47, 45.63), 
@@ -16,17 +17,18 @@ map_crime <- function(df, map, chart_title, s = 1, a = 1) {
 
 aa <- pers_df[pers_df$major_offense_type == "Robbery" & 
                 year(pers_df$report_date) == 2010,]
-bb <- full_df[full_df$major_offense_type == "Homicide",]
-bb$report_date <- as.factor(year(bb$report_date))
+
+bb <- full_df[full_df$major_offense_type == "Motor Vehicle Theft" &
+                year(full_df$report_date) == 2010,]
+bb$report_date <- as.factor(month(bb$report_date))
 
 map_crime(aa, portland_map, "Robbery", s = 1, a = .4)
 
 map_crime2 <- function(df, map, chart_title, s = 1, a = 1) {
   ggmap(map) +
-    geom_point(data = df, aes(x = lat, y = long, 
-                              color = report_date),
+    geom_point(data = df, aes(x = lat, y = long),
                size = s, alpha = a) +
     labs(title = chart_title)
 }
 
-map_crime2(bb, portland_map, "Homicides 2004-2014", s = 2, a = .7)
+map_crime2(bb, portland_map, "Motor Vehicle Thefts 2010", s = .75, a = .5)
